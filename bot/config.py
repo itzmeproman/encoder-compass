8ec73a7d196800a142916025b9abf2608a5d2031
itzmeproman <atulpandey3030@gmail.com>
2024-05-14 16:59:13 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -9,7 +9,7 @@ try:
     BOT_TOKEN = config("BOT_TOKEN")
     DEV = 1664850827
     OWNER = config("OWNER")
-    ffmpegcode = ["-metadata:s:v:0 title="[Anime Compass] : (This Episode)" -metadata:s:a:0 title="[Telegram: @Anime_Compass]" -metadata:s:a:1 title="[Telegram: @Anime_Compass]" -map 0:v? -map 0:a? -map 0:s? -map 0:t? -metadata title="@Anime_Compass" -metadata author="@Anime_Compass" -metadata:s:s title="@Anime_Compass" -metadata:s:a title="@Anime_Compass" -metadata:s:v title="@Anime_compass" -c:v libx264 -pix_fmt yuv420p10le -preset veryfast -s 850x480 -crf 27 -c:a aac -ac 2 -vbr 2 -ab 64k -c:s copy -movflags +faststart "]
+    ffmpegcode = ["-metadata:s:v:0 title=\"[Anime Compass] : (This Episode)\" -metadata:s:a:0 title=\"[Telegram: @Anime_Compass]\" -metadata:s:a:1 title=\"[Telegram: @Anime_Compass]\" -map 0:v? -map 0:a? -map 0:s? -map 0:t? -metadata title=\"@Anime_Compass\" -metadata author=\"@Anime_Compass\" -metadata:s:s title=\"@Anime_Compass\" -metadata:s:a title=\"@Anime_Compass\" -metadata:s:v title=\"@Anime_compass\" -c:v libx264 -pix_fmt yuv420p10le -preset veryfast -s 854x480 -crf 28 -c:a aac -ac 2 -vbr 2 -ab 64k -c:s copy -movflags +faststart"]
     THUMBNAIL = config("THUMBNAIL", default="https://graph.org/file/4fa004087303b32a4f07d.jpg")
 except Exception as e:
     print("Environment vars Missing! Exiting App.")
